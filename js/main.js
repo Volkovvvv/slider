fdb66f6b5aba472058d06b099a4224eff0e43e5b
Volkovvvv <vladvolkovvv@mail.ru>
2020-05-12 14:18:04 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,38 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
   });
+  $(function(){
+    $('.perepel').hover(function(){
+      $('.per-sims').toggleClass('block'); 
+    })
+    $('.shastic-slide').hover(function(){
+      $('.shastic-sims').toggleClass('block'); 
+    })
+    $('.sorok').hover(function(){
+      $('.sorok-sims').toggleClass('block'); 
+    })
+    $('.pluxsch').hover(function(){
+      $('.plux').toggleClass('block'); 
+    })
+    $('.lab').hover(function(){
+      $('.labocki').toggleClass('block'); 
+    })
+    $('.urcha__wrapper').hover(function(){
+      $('.yurcha-sims').toggleClass('block'); 
+    })
+    $('.sherb__wrapper').hover(function(){
+      $('.sherb-sims').toggleClass('block'); 
+    })
+    $('.pavlova__wrapper').hover(function(){
+      $('.pavlova-sims').toggleClass('block'); 
+    })
+    $('.gvaeva__wrapper').hover(function(){
+      $('.gvaeva-sims').toggleClass('block'); 
+    })
+    $('.sharkova__wrapper').hover(function(){
+      $('.sharkova-sims').toggleClass('block'); 
+    })
+    $('.zhurko__wrapper').hover(function(){
+      $('.zhurko-sims').toggleClass('block'); 
+    })
+});
